054d29ce808132e2089a166364d13e5afcca5cd2
Patrick <markpatrickperdon@gmail.com>
2020-05-30 18:43:27 +0800

    Added table view delegate method for selecting row in table Added fuctionality if row is selected add checkmark to row by using cell.accessory property

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,25 +10,50 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Item1", "Item2", "Item3"]
+    var itemArray = ["Item1", "Item2", "Item3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
 }
 
+//MARK: Table View Datasource Methods
 extension TodoListViewController{
+   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: Const.todoCellID, for: indexPath)
+    cell.textLabel?.text = itemArray[indexPath.row]
+       return cell
+   }
    
+   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return itemArray.count
+   }
+}
+
+//MARK: Table View Delegate Methods
+extension TodoListViewController{
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+//        if itemArray[indexPath.row].isChecked == false{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//            itemArray[indexPath.row].isChecked = true
+////            tableView.reloadData()
+//        }else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//            itemArray[indexPath.row].isChecked = false
+////            tableView.reloadData()
+//        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 }
 
