f0a685a2d402ae27c52cac3553ba606086d0116d
Patrick <markpatrickperdon@gmail.com>
2020-05-30 14:39:18 +0800

    Added Constants Files Created a itemArray in TodolistVC Added tableyview methods needed for displaying itemsArray on screen

---
+++ Todoey/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Mark Patrick Perdon on 5/30/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+enum Const{
+    static let todoCellID = "TodoItemCell"
+}
--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Item1", "Item2", "Item3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+}
 
-
+extension TodoListViewController{
+   
 }
 
