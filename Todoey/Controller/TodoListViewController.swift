dceab423e0704f584cd44acb3f2977ca5422181f
Patrick <markpatrickperdon@gmail.com>
2020-06-01 18:39:27 +0800

    Model Folder was renamed to Views (there's already a model folder) removed code relating to user defaults Data persistence implemented using PropertyList Encoder and Decoder

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -11,53 +11,33 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-//                    [Item(name: "Item1"),
-//                     Item(name: "Item2"),
-//                     Item(name: "Item3"),
-//                     Item(name: "a"),
-//                     Item(name: "b"),
-//                     Item(name: "c"),
-//                     Item(name: "t"),
-//                     Item(name: "h"),
-//                     Item(name: "e"),
-//                     Item(name: "q"),
-//                     Item(name: "u"),
-//                     Item(name: "i"),
-//                     Item(name: "c"),
-//                     Item(name: "k"),
-//                     Item(name: "b"),
-//                     Item(name: "r"),
-//                     Item(name: "o"),
-//                     Item(name: "w"),
-//                     Item(name: "n"),
-//                     Item(name: "f"),
-//                     Item(name: "o"),
-//                     Item(name: "x"),
-//                     Item(name: "j"),
-//                     Item(name: "u"),
-//                     Item(name: "m"),
-//                     Item(name: "p"),
-//                     Item(name: "s"),
-//                     Item(name: "o"),
-//                     Item(name: "v"),
-//                     Item(name: "e"),
-//                     Item(name: "r"),
-//                     Item(name: "t"),
-//                     Item(name: "h"),
-//                     Item(name: "e"),
-//                     Item(name: "l"),
-//                     Item(name: "a"),
-//                     Item(name: "z"),
-//                     Item(name: "y"),
-//                     Item(name: "d"),
-//                     Item(name: "o"),
-//                     Item(name: "g")]
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("test.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let itemsArray = defaults.array(forKey: Const.itemsArrayKey) as? [Item]{
-            self.itemArray = itemsArray
+        loadItems()
+        
+    }
+    
+    func saveItem(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            print("Error encoding: \(error)")
+        }
+    }
+    
+    func loadItems(){
+        let decoder = PropertyListDecoder()
+        if let data = try? Data(contentsOf: dataFilePath!){
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print(error)
+            }
+            
         }
     }
 }
@@ -83,6 +63,7 @@ extension TodoListViewController{
 extension TodoListViewController{
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].isChecked = !itemArray[indexPath.row].isChecked
+        
         tableView.deselectRow(at: indexPath, animated: true)
         tableView.reloadRows(at: [indexPath], with: .fade)
         
@@ -104,13 +85,12 @@ extension TodoListViewController{
         
         let addNote = UIAlertAction(title: "Add Item", style: .default){ action in
             if let newNote = note.text, !newNote.isEmpty{
+                let newItem = Item(name: newNote)
                 let indexPath = IndexPath(row: self.itemArray.count, section: 0)
-                let item = Item(name: newNote)
                 
-                self.itemArray.append(item)
-                //                let encodedData = NSKeyedArchiver
+                self.itemArray.append(newItem)
+                self.saveItem()
                 
-                self.defaults.set(self.itemArray, forKey: Const.itemsArrayKey )
                 self.tableView.insertRows(at: [indexPath], with: .automatic)
                 note.text = ""
             }
--- Todoey/Model/Item.swift
+++ Todoey/Model/Item.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Item {
+struct Item: Codable {
     var name: String
     var isChecked: Bool = false
 }
