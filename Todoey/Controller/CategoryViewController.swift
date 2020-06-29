6d726f772e793fa6864354033361808fb327a3a3
Patrick <markpatrickperdon@gmail.com>
2020-06-29 20:19:39 +0800

    Implemented loading and saving of categories

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -11,9 +11,12 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
     var categoriesArray = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//          print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadCategories()
+        //          print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -22,20 +25,69 @@ class CategoryViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+        do{
+            categoriesArray = try context.fetch(request)
+        }catch{
+            print(error)
+        }
     }
     
-    // MARK: - Table view data source
-    
+    func saveCategory(){
+        do{
+            try context.save()
+        }catch{
+            print("Error saving: \(error)")
+        }
+    }
+}
+
+// MARK: - Table view data source
+extension CategoryViewController{
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categoriesArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        
+        cell.textLabel?.text = categoriesArray[indexPath.row].categoryName
         return cell
     }
+}
+
+// MARK: - Table View Delegate Methods
+extension CategoryViewController{
     
-    // MARK: - Table View Delegate Methods
+}
+
+//MARK: - Bar Button Item
+extension CategoryViewController{
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let addCategoryAC = UIAlertController(title: "Add Catergory",
+                                              message: "Type in a category",
+                                              preferredStyle: .alert)
+        
+        var categoryText = UITextField()
+        
+        addCategoryAC.addTextField { (textfield) in
+            textfield.placeholder = "Groceries, Reminders, etc."
+            categoryText = textfield
+        }
+        
+        let addCategoryAction = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            if let text = categoryText.text, !text.isEmpty{
+                let newCategory = Category(context: self.context)
+                let indexPath = IndexPath(item: self.categoriesArray.count, section: 0)
+                
+                newCategory.categoryName = text
+                self.categoriesArray.append(newCategory)
+                self.saveCategory()
+                
+                self.tableView.insertRows(at: [indexPath], with: .automatic)
+                categoryText.text = ""
+            }
+        }
+        
+        addCategoryAC.addAction(addCategoryAction)
+        present(addCategoryAC, animated: true)
+    }
 }
